d789d2a1aefdc2ecb2fe69b30cf057409e7a400c
mmh1196 <maricarmen.morales@solera.com>
2022-09-07 13:45:24 -0500

    added initial commit

---
+++ src/main/java/org/demosolera/firts_demo_soleraproject1/App.java
@@ -0,0 +1,13 @@
+package org.demosolera.firts_demo_soleraproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/demosolera/firts_demo_soleraproject1/AppTest.java
@@ -0,0 +1,20 @@
+package org.demosolera.firts_demo_soleraproject1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
